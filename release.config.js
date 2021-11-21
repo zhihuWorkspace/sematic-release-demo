f48e575c253be4e90c7ed0614d9005021e15410f
Claire Qian <v.claire.qian@welab.co>
2021-11-21 15:55:22 +0800

    fix(ci): release

--- release.config.js
+++ release.config.js
@@ -5,12 +5,11 @@ module.exports = {
     '@semantic-release/release-notes-generator', 
     ["@semantic-release/npm", {
       "npmPublish": false,
-      "tarballDir": "dist",
     }],
     '@semantic-release/github',
     ['@semantic-release/git', {
       // "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      "message": "chore(release): ${nextRelease.version}"
     }]
   ]
 }
