f2575e94a3caa4dd31d7b79541957065c1c67408
Claire Qian <v.claire.qian@welab.co>
2021-11-22 18:25:38 +0800

    feat(ci): test channel

--- release.config.js
+++ release.config.js
@@ -2,8 +2,8 @@ module.exports = {
   tagFormat: 'v${version}',
   "branches": [
     {name: 'master'}, // `channel` is undefined so the default distribution channel will be used
-    {name: 'pre/rc', channel: 'pre/rc', prerelease: 'rc'}, // `prerelease` is built with the template `${name.replace(/^pre\\//g, "")}`
-    {name: 'beta', channel: 'beta', prerelease: 'beta'}, // `prerelease` is set to `beta` as it is the value of `name`
+    // {name: 'pre/rc', channel: 'pre/rc', prerelease: 'rc'}, // `prerelease` is built with the template `${name.replace(/^pre\\//g, "")}`
+    // {name: 'beta', channel: 'beta', prerelease: 'beta'}, // `prerelease` is set to `beta` as it is the value of `name`
   ],
   "plugins": [
     '@semantic-release/commit-analyzer', 
